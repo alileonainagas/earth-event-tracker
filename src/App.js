f2a55cf6dd4da5a3afe34a8e63e90cde9b78277d
Alí León Ainagas <61817233+alileonainagas@users.noreply.github.com>
2020-12-02 23:25:35 -0400

    New Routing Schema

--- src/App.js
+++ src/App.js
@@ -7,8 +7,10 @@ import favIcon from './assets/favicon.ico';
 import logo192 from './assets/logo192.png';
 
 //Components
-import MapView from './components/MapView';
-import Home from "./components/Home";
+import MapViewEONET from './views/mapeonet/MapViewEONET';
+import MapViewISS from './views/mapiss/MapViewISS';
+import Home from './components/Home';
+import Menu from './components/Menu';
 import Header from './components/Header';
 import Footer from './components/Footer';
 
@@ -18,24 +20,20 @@ function App() {
 		<HelmetProvider>
 			<Router>
 				<Helmet>
-				{/* Public Metadata implementation */}
-				<title>Earth Event Tracker</title>
-				<link rel="icon" type="image/png" href={favIcon} sizes="32x32" />
-				<link rel="apple-touch-icon" href={logo192} />
-				<meta name="description" content="React Web App created to events taking place in real time in the world." />
-				</Helmet> 
+					{/* Public Metadata implementation */}
+					<title>Earth Event Tracker</title>
+					<link rel="icon" type="image/png" href={favIcon} sizes="32x32" />
+					<link rel="apple-touch-icon" href={logo192} />
+					<meta name="description" content="React Web App created to events taking place in real time in the world." />
+				</Helmet>
+				<Header />
 				<Switch>
-					<Route exact path='/'>
-						<Header />
-						<Home />
-						<Footer />
-					</Route>
-					<Route path='/map'>
-						<Header />
-						<MapView />
-						<Footer />
-					</Route>
+					<Route exact path='/' component={Home} />
+					<Route exact path='/trackers' component={Menu} />
+					<Route path='/wilfire-map' component={MapViewEONET} />
+					<Route path='/iss-map' component={MapViewISS} />
 				</Switch>
+				<Footer />
 			</Router>
 		</HelmetProvider>
 	);
