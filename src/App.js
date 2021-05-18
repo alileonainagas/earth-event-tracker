eef65e9116856e7d41be07711bf2a45bd1f543bd
Alí León Ainagas <61817233+alileonainagas@users.noreply.github.com>
2021-05-18 18:45:42 -0400

    Try #1 BrowserRouter deployment

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { HashRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 //Assets
