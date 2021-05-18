cdacf9e6f2fcb8fcde8e5a8fe3f73075ecb8f050
Alí León Ainagas <61817233+alileonainagas@users.noreply.github.com>
2021-05-18 18:37:32 -0400

    Hashrouter Added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 //Assets
